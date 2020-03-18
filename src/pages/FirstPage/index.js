340db3f57c4f7d6e7500ad42562c8f95f49c50ab
walisonmatheus <walisom360@hotmail.com>
2020-03-17 22:19:42 -0300

    adjusting

--- src/pages/FirstPage/index.js
+++ src/pages/FirstPage/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { Container,Background } from './styles';
+import { Container,Background,ButtonNext } from './styles';
 
 import img from '../../assets/Tela-Inicial-1.1.png';
+
 
 export default function FirstPage() {
   return (
     <Container>
       <Background banner={img}  />
+      <ButtonNext>Iniciar</ButtonNext>
     </Container>
   );
 }
--- src/pages/FirstPage/styles.js
+++ src/pages/FirstPage/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
 `;
 
 export const Background = styled.div`
- 
+  margin-top:320px;
 
  
   background: url(${props => (props.banner)}) no-repeat;
@@ -22,7 +22,44 @@ export const Background = styled.div`
  
   object-fit: scale-down;
   z-index: -3;
+
+  position:absolute;
 `;
 
 
 
+
+
+export const ButtonNext = styled.button`
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  &:disabled {
+    opacity: 0.5;
+    cursor: default;
+  }
+  p {
+    margin-right: 6px;
+  }
+  border: none;
+  height: 80px;
+  border-radius: 25px;
+  cursor: pointer;
+  color: white;
+  background: #755684;
+  width: 270px;
+  font-size: 32px;
+  font-weight: bold;
+  position:absolute;
+  margin-top:550px;
+  outline: none;
+  margin-left: 10px;
+  margin-right: 32px;
+  &:hover {
+    background: #72159c;
+    &:disabled {
+      opacity: 0.5;
+      cursor: default;
+    }
+  }
+`;
