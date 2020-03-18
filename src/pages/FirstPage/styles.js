9fe01a119c73a80d942b432382c132ff88f920f7
walisonmatheus <walisom360@hotmail.com>
2020-03-17 22:07:29 -0300

    adjusting

--- src/pages/FirstPage/styles.js
+++ src/pages/FirstPage/styles.js
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width:100%;
   height:100%;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  margin-top:60px;
 `;
 
 export const Background = styled.div`
@@ -13,9 +17,9 @@ export const Background = styled.div`
   background: url(${props => (props.banner)}) no-repeat;
   background-size: 100% 100%;
  
-  width: 100%;
-  height: 100%;
-  position: absolute;
+  width: 1000px;
+  height: 700px;
+ 
   object-fit: scale-down;
   z-index: -3;
 `;
